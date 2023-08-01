473888460da1fafd93589c0fc94a10561889b275
Andre <andreferreira@live.co.uk>
2023-08-01 23:16:36 +0100

    updating README file and last layout tweaks

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const original_price = 169.99;
 originalPriceClass.textContent = `$${original_price}`;
 promoPriceClass.textContent = `$${original_price - 20}`;
 
-window.addEventListener("resize", () => {
+const checkWindowSize = () => {
   const productImageClass = document.querySelector(".product-image");
-
-  if (this.innerWidth >= 1440) {
+  if (window.innerWidth >= 1440) {
     productImageClass.src = "./images/image-product-desktop.jpg";
   } else {
     productImageClass.src = "./images/image-product-mobile.jpg";
   }
-});
+};
+
+document.addEventListener("DOMContentLoaded", checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
